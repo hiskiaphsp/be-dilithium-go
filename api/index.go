e47fcc9932b357a6fe1d570e9f836919daefab47
Hiskia Parhusip <hiskiaparhusip@gmail.com>
2024-07-14 15:11:43 +0700

    add handler

---
+++ api/index.go
@@ -0,0 +1,67 @@
+package handler
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	"be-dilithium/config"
+	"be-dilithium/controllers"
+	controller "be-dilithium/controllers"
+	"be-dilithium/repositories"
+	"be-dilithium/services"
+	service "be-dilithium/services"
+)
+
+// Handler initializes and runs the Gin server.
+func Handler() *gin.Engine {
+	// Ensure MongoDB connection is available
+	if config.MongoDB == nil {
+		log.Fatal("MongoDB connection is not available")
+	}
+
+	// Create database and collection instances
+	db := config.MongoDB.Database(config.DBName)
+	documentRepo := repositories.NewDocumentRepository(db, "documents")
+	documentService := services.NewDocumentService(documentRepo)
+	documentController := controllers.NewDocumentController(documentService, os.Getenv("PUBLIC_STORAGE"))
+	// Initialize the Dilithium controller with a factory function for creating the service
+	dilithiumController := controller.NewDilithiumController(service.NewDilithiumService)
+
+	// Initialize Gin router
+	router := gin.Default()
+
+	router.Static("public/storage", "public/storage")
+
+	// Create API v1 group
+	apiV1 := router.Group("/api/v1")
+	{
+		apiV1.POST("/generate-keypair", dilithiumController.GenerateKeyPair)
+		apiV1.POST("/sign-message", dilithiumController.SignMessage)
+		apiV1.POST("/sign-message-url", dilithiumController.SignMessageUrl)
+		apiV1.POST("/verify-signature", dilithiumController.VerifySignature)
+		apiV1.POST("/verify-signature-url", dilithiumController.VerifySignatureURL)
+		apiV1.POST("/analyze", dilithiumController.AnalyzeExecutionTimeAndSizes)
+		apiV1.POST("/analyze-url", dilithiumController.AnalyzeExecutionTimeAndSizesUrl)
+
+		// Document routes
+		apiV1.POST("/documents", documentController.CreateDocument)
+		apiV1.GET("/documents/:id", documentController.GetDocumentByID)
+		apiV1.GET("/documents", documentController.GetAllDocuments)
+		apiV1.PUT("/documents", documentController.UpdateDocument)
+		apiV1.DELETE("/documents/:id", documentController.DeleteDocument)
+	}
+
+	return router
+}
+
+func main() {
+	router := Handler()
+	// Run server
+	port := config.Port
+	if port == "" {
+		port = "80"
+	}
+	router.Run(":" + port)
+}
